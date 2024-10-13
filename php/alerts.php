a3673dc57a2d45a55fb65a0d21152cc2df9a4f1d
StrwbrryMochi <momoiroyuki629@gmail.com>
2024-10-13 22:27:12 +0800

    Added Alerts

---
+++ php/alerts.php
@@ -0,0 +1,25 @@
+<?php 
+
+if (isset($_GET['add_slot']) && $_GET['add_slot'] == 'true') {
+    echo "<script>
+        toastr.success(' ', 'Slot Added');
+
+        toastr.options = {
+            closeButton: true,
+            debug: false,
+            newestOnTop: false,
+            progressBar: false,
+            positionClass: 'toast-top-right',
+            preventDuplicates: true,
+            onclick: null,
+            showDuration: '300',
+            hideDuration: '1000',
+            timeOut: '5000',
+            extendedTimeOut: '1000',
+            showEasing: 'swing',
+            hideEasing: 'linear',
+            showMethod: 'fadeIn',
+            hideMethod: 'fadeOut'
+        };
+    </script>";
+}
