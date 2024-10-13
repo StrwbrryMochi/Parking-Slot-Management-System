d648c5a0a5837b6a01418c4cc07605d2b2a50603
StrwbrryMochi <momoiroyuki629@gmail.com>
2024-10-13 22:24:19 +0800

    Separated the Cards to be Included Wherever

---
+++ components/cards.php
@@ -0,0 +1,38 @@
+<div class="cards-container">
+        <div class="cards total">
+            <div class="card-info">
+                <div class="card-data">300</div>
+                <div class="card-title">Total Slots</div>
+            </div>
+            <div class="card-icon">
+                <i class="fas fa-th"></i>
+            </div>
+        </div>
+        <div class="cards available">
+            <div class="card-info">
+                <div class="card-data">299</div>
+                <div class="card-title">Available Slots</div>
+            </div>
+            <div class="card-icon">
+                <i class="fas fa-parking"></i>
+            </div>
+        </div>
+        <div class="cards occupied">
+            <div class="card-info">
+                <div class="card-data">300</div>
+                <div class="card-title">Occupied Slots</div>
+            </div>
+            <div class="card-icon">
+                <i class="fas fa-car"></i>
+            </div>
+        </div>
+        <div class="cards reserved">
+            <div class="card-info">
+                <div class="card-data">0</div>
+                <div class="card-title">Reserved Slots</div>
+            </div>
+            <div class="card-icon">
+                <i class="fas fa-user-lock"></i>
+            </div>
+        </div>
+    </div>
