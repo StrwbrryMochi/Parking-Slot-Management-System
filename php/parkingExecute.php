24190cee56731453b3df63a2d007889b5b344529
StrwbrryMochi <momoiroyuki629@gmail.com>
2024-10-16 19:30:24 +0800

    Added slot Validation and added Edit Function

--- php/parkingExecute.php
+++ php/parkingExecute.php
@@ -5,7 +5,7 @@
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // For Adding Slot
-        if (isset($_POST['addParking'])) {
+        if (isset($_POST['addSlot'])) {
             
             $floor = $_POST['floor'];
             $zone = $_POST['zone'];
@@ -14,10 +14,34 @@
             $vehicleType = $_POST['vehicle_type'];
             $userType = $_POST['user_type'];
             
-            // Get the current page from the form
             $current_page = isset($_POST['current_page']) ? $_POST['current_page'] : 'default';
     
-            // Call Function To Add A Slot
-            parkingAdd($floor, $zone, $slot_number, $plateNumber, $vehicleType, $userType, $current_page);
+            if ($floor !== null && $zone !== null && $slot_number !== null && $plateNumber !== null && $vehicleType !== null && $userType !== null) {
+                // Call Function To Add A Slot
+                slotAdd($floor, $zone, $slot_number, $plateNumber, $vehicleType, $userType, $current_page);
+            } else {
+                echo "Error: Missing or invalid data.";
+            }
         }
+
+        if (isset($_POST['editSlot'])) {
+
+            $floor = $_POST['floor'];
+            $zone = $_POST['zone'];
+            $slot_number = $_POST['slot_number'];
+            $plateNumber = $_POST['plate_number'];
+            $vehicleType = $_POST['vehicle_type'];
+            $userType = $_POST['user_type'];
+
+            $current_page = isset($_POST['current_page']) ? $_POST['current_page'] : 'default';
+
+             if ($floor !== null && $zone !== null && $slot_number !== null && $plateNumber !== null && $vehicleType !== null && $userType !== null) {
+                // Call the slotEdit function to update the slot information
+                slotEdit($floor, $zone, $slot_number, $plateNumber, $vehicleType, $userType, $current_page);
+            } else {
+                echo "Error: Missing or invalid data.";
+            }
+        }
+
+
     }
