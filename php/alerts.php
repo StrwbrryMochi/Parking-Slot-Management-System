acf93b1a1766f4bf1637c366db11350ca5dcb659
StrwbrryMochi <momoiroyuki629@gmail.com>
2024-10-16 19:29:44 +0800

    Added Edit Alert

--- php/alerts.php
+++ php/alerts.php
@@ -23,3 +23,27 @@ if (isset($_GET['add_slot']) && $_GET['add_slot'] == 'true') {
         };
     </script>";
 }
+
+if (isset($_GET['edit_slot']) && $_GET['edit_slot'] == 'true') {
+    echo "<script>
+        toastr.success(' ', 'Slot Edited');
+
+        toastr.options = {
+            closeButton: true,
+            debug: false,
+            newestOnTop: false,
+            progressBar: false,
+            positionClass: 'toast-top-right',
+            preventDuplicates: true,
+            onclick: null,
+            showDuration: '300',
+            hideDuration: '1000',
+            timeOut: '5000',
+            extendedTimeOut: '1000',
+            showEasing: 'swing',
+            hideEasing: 'linear',
+            showMethod: 'fadeIn',
+            hideMethod: 'fadeOut'
+        };
+    </script>";
+}
