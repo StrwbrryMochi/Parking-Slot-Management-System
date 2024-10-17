a819ca18bcbfcc806972da4e44d36327cbe663a2
StrwbrryMochi <momoiroyuki629@gmail.com>
2024-10-17 23:41:00 +0800

    Added Boilerplate and Function for Checkout

--- php/parkingExecute.php
+++ php/parkingExecute.php
@@ -42,6 +42,28 @@
                 echo "Error: Missing or invalid data.";
             }
         }
+        
+        if (isset($_POST['checkoutSlot'])) {
 
+            $floor = $_POST['floor'];
+            $zone = $_POST['zone'];
+            $slot_number = $_POST['slot_number'];
+            $plateNumber = $_POST['plate_number'];
+            $vehicleType = $_POST['vehicle_type'];
+            $userType = $_POST['user_type'];
+            $status = $_POST['status'];
+            $time_in = $_POST['time_in'];
+            $time_out = $_POST['time_out'];
+            $duration = $_POST['duration'];
+            $fee = $_POST['fee'];
 
+            $current_page = isset($_POST['current_page']) ? $_POST['current_page'] : 'default';
+
+            if ($floor !== null && $zone !== null && $slot_number !== null && $plateNumber !== null && $vehicleType !== null && $userType !== null && $status !== null && $time_in !== null && $time_out !== null && $duration !== null && $fee !== null) {
+                // Call the checkoutSlot function to update the slot information
+                slotCheckout($floor, $zone, $slot_number, $plateNumber, $vehicleType, $userType, $status, $time_in, $time_out, $duration, $fee, $current_page);
+            } else {
+                echo "Error: Missing or invalid data.";
+            }
+        }
     }
