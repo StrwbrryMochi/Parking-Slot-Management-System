6401ca4c6bd56dea6e8cec4b4c3357944b0ebd07
StrwbrryMochi <momoiroyuki629@gmail.com>
2024-10-17 23:38:12 +0800

    Added Hidden Inputs to Store Corresponding Data

--- components/checkoutModal.php
+++ components/checkoutModal.php
@@ -7,21 +7,23 @@
     aria-labelledby="checkoutModalLabel"
     aria-hidden="true"
 >
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modalTitleId">
-                    Modal title
-                </h5>
+    <div class="modal-dialog checkout" role="document">
+        <div class="modal-content checkout">
+            <div class="checkout-modal-header">
                 <button
-                    type="button"
-                    class="btn-close"
-                    data-bs-dismiss="modal"
-                    aria-label="Close"
-                ></button>
+                type="button"
+                class="btn-close custom-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+                >
+                <i class="fa-solid fa-circle-dot"></i>
+                </button>
             </div>
-            <div class="modal-body">
-                <form action="">
+            <div class="modal-body checkout">
+                <div class="slot-barcode-container">
+                </div>
+                <div class="checkout-data-container">
+                <form action="../php/parkingExecute.php" method="POST">
                     <div class="checkout-slot-data">
                     <input type="text" id="display-floor-checkout" name="floor" readonly>
                     <input type="text" id="display-zone-checkout" name="zone" readonly>
@@ -33,12 +35,16 @@
                     <p id="checkout-time-in"></p>
                     <p id="checkout-time-out"></p>
                     <p id="checkout-duration"></p>
+                    <p id="checkout-fee"></p>
                     <input type="hidden" id="hidden-license-plate-checkout" name="plate_number">
                     <input type="hidden" id="hidden-user-type" name="user_type">
-                    <input type="hidden" id="hidden-vehicle-type" name="vehicle_type">                
+                    <input type="hidden" id="hidden-vehicle-type" name="vehicle_type">        
+                    <input type="hidden" id="hidden-status" name="status">        
+                    <input type="hidden" id="form-time-in" name="time_in">             
                     <input type="hidden" id="hidden-time-out" name="time_out">                    
-                    <input type="hidden" id="hidden-duration" name="duration">                    
-                    </form>
+                    <input type="hidden" id="hidden-duration" name="duration">               
+                    <input type="hidden" id="hidden-fee" name="fee">               
+                </div>  
             </div>
             <div class="modal-footer">
                 <button
@@ -48,8 +54,9 @@
                 >
                     Close
                 </button>
-                <button type="button" class="btn btn-primary">Save</button>
+                <button type="submit" class="btn btn-primary" name="checkoutSlot">Save</button>
             </div>
         </div>
+        </form>
     </div>
 </div>
