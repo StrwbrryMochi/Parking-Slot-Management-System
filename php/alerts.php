21a8abfccb76aa2043cd8de62917458d56a72ed7
StrwbrryMochi <momoiroyuki629@gmail.com>
2024-10-22 10:05:20 +0800

    added Login and Register alerts

--- php/alerts.php
+++ php/alerts.php
@@ -1,5 +1,44 @@
 <?php 
 
+//* Login & Register
+// Welcome Staff
+if (isset($_GET['welcome_user']) && $_GET['welcome_user'] == 'true') {
+    $profilePhoto = $Photo;  
+    
+    echo "<script>
+            setTimeout(function() {
+            var username = '" . $FirstName . "';
+            var profilePhoto = '" . $Photo . "'; 
+
+            Swal.fire({
+                position: 'top',
+                title: '<span style=\"color: white;\">Welcome, </span><span style=\"color: #53539b; margin: 0 5px;\">' + username + '</span><span style=\"color: white;\">!</span>',
+                showConfirmButton: false,
+                timer: 2000,
+                html: '<img src=\"' + profilePhoto + '\" style=\"width: 100px; height: 100px; border-radius: 50%; object-fit: cover;\">',
+                background: 'rgb(18,21,31,255)',
+            });
+        }, 5000); 
+    </script>";
+}
+
+if (isset($_GET['register_success']) && $_GET['register_success'] == 'true') {
+    echo "<script>
+            setTimeout(function() {
+            var username = '" . $FirstName . "';
+
+            Swal.fire({
+                icon: 'success',
+                title: 'Success',
+                text: 'Account successfully created,' + username + '!',
+                timer: 2000
+            });
+        }, 5000);      
+    </script>";
+}
+
+
+//* Parking Slot Management
 // Add
 if (isset($_GET['add_slot']) && $_GET['add_slot'] == 'true') {
     echo "<script>
