5fea7a953d7a9e17eb7a8caa3551a301d9ca44b8
StrwbrryMochi <momoiroyuki629@gmail.com>
2024-10-22 10:50:55 +0800

    Refactored

--- js/modal.js
+++ js/modal.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Check if the elements exist
   if (snippetButton && snippetContainer && snippetOverlay) {
-    // Add a click event listener to the snippetButton
     snippetButton.addEventListener("click", function () {
       if (
         snippetContainer.style.display === "none" ||
@@ -15,26 +14,26 @@ document.addEventListener("DOMContentLoaded", function () {
         snippetContainer.style.display = "block";
 
         setTimeout(() => {
-          snippetOverlay.style.opacity = "1"; // Fade in overlay
-          snippetContainer.style.opacity = "1"; // Fade in snippet container
+          snippetOverlay.style.opacity = "1";
+          snippetContainer.style.opacity = "1";
         }, 10);
       }
     });
 
     // Add a click event listener to the snippetOverlay to close both elements
     snippetOverlay.addEventListener("click", function () {
-      snippetOverlay.style.opacity = "0"; // Fade out overlay
-      snippetContainer.style.opacity = "0"; // Fade out snippet container
+      snippetOverlay.style.opacity = "0";
+      snippetContainer.style.opacity = "0";
 
       setTimeout(() => {
-        snippetOverlay.style.display = "none"; // Hide overlay
-        snippetContainer.style.display = "none"; // Hide snippet container
-      }, 300); // Match with the CSS transition duration
+        snippetOverlay.style.display = "none";
+        snippetContainer.style.display = "none";
+      }, 300);
     });
 
     // Add a click event listener to the snippetContainer to stop propagation
     snippetContainer.addEventListener("click", function (event) {
-      event.stopPropagation(); // Prevent click from closing the overlay
+      event.stopPropagation();
     });
   }
 });
